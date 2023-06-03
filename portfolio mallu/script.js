010078ce2a758b2acb851e36fef5c1355c2585f5
Mallikarjun Teli <107787725+mallikarjunteli@users.noreply.github.com>
2023-06-03 16:25:17 +0530

    Add files via upload

---
+++ portfolio mallu/script.js
@@ -0,0 +1,5 @@
+var cv = document.getElementsByClassName('btn');
+var cvLink = 'https://drive.google.com/file/d/147lsMZijmCkF2ZTT0-h-CoglXneJ389x/view?usp=sharing'
+cv.addEventListener('click', function(){
+    window.open(cvLink);
+})
